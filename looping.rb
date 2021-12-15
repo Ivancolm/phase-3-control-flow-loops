fa39735136021aa6dec161fb061c35450606b609
ivancolmenares <ivancolmenares@live.com>
2021-12-14 20:51:20 -0600

    commit tree 854310a081ab01ba69a7ce6357388bcbf5f5ae5e

--- looping.rb
+++ looping.rb
@@ -1,24 +1,32 @@
 def happy_new_year
-  # your code here
+  10.times do |num|
+    while (counter > 0)
+      counter--;
 end
 
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
-def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
-  else
-    num
-  end
-end
+# # No need to modify this code! Use this to implement the fizzbuzz_printer method.
+# def fizzbuzz(num)
+#   if num % 3 == 0 && num % 5 == 0
+#     "FizzBuzz"
+#   elsif num % 3 == 0
+#     "Fizz"
+#   elsif num % 5 == 0
+#     "Buzz"
+#   else
+#     num
+#   end
+# end
 
 def fizzbuzz_printer
-  # your code here
+  for (let num = 1; num <= 100; num++) {
+    fizzbuzzprinter(num);
+  }
 end
 
 def reverse_string(str)
-  # your code here
+  str = "";
+  for (let i = 0; i < str.length; i++) {
+    reversedStr = str[i] + reversedStr;
+  }
+  return reversedStr;
 end
